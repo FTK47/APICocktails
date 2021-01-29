88b8ce46f9c5ff82214fe429f726000d73d72c05
FTK47 <ftkoch2001@gmail.com>
2021-01-29 09:51:01 +0100

    Funktioner

--- CocktailsData.py
+++ CocktailsData.py
@@ -1,12 +1,41 @@
 import requests, json
 
-ingredientOptions = requests.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
-print(ingredientOptions)
+def getById(idDrink):
+    data = requests.get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + idDrink)
+    if data.status_code == 200 and len(data.text) > 0:
+        jsonData = json.loads(data.text)
+        return jsonData['drinks']
+    else:
+        return []
 
-ingredient = 'Gin'
+def getByIngredient(ingredient):
+    data = requests.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + ingredient)
+    if data.status_code == 200 and len(data.text) > 0:
+        jsonData = json.loads(data.text)
+        return jsonData['drinks']
+    else:
+        return []
 
-data = requests.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + ingredient)
-jsonData = json.loads(data.text)
+def findDuplicates(listOfLists):
+    countDict = {}
+    for list in listOfLists:
+        for cocktail in list:
+            if cocktail['idDrink'] in countDict.keys():
+                countDict[cocktail['idDrink']] += 1
+            else:
+                countDict[cocktail['idDrink']] = 1
+    return countDict
 
-print('Status Code', data.status_code)
-print(jsonData)
+def search(ingredients):
+    results = []
+    for ingredient in ingredients:
+        results.append(getByIngredient(ingredient))
+    findDuplicates(results)
+
+search(['Gin', 'Vodka'])
+#print(getById('11007'))
+#print(getByIngredient('Vermouth'))
+#print(getByIngredient('Gin'))
+#print('Status Code', data.status_code)
+#print(jsonData)
+#{'strDrink': 'Addison', 'strDrinkThumb': 'https://www.thecocktaildb.com/images/media/drink/yzva7x1504820300.jpg', 'idDrink': '17228'}
