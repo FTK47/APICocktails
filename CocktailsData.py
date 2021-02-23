d1f2c5f4e8f99c129e2f9a2642478b2289a5aac2
FTK47 <ftkoch2001@gmail.com>
2021-02-23 08:37:44 +0100

    jg

--- CocktailsData.py
+++ CocktailsData.py
@@ -1,4 +1,4 @@
-import requests, json
+import requests, json, pprint
 
 def getById(idDrink):
     data = requests.get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + idDrink)
@@ -12,7 +12,7 @@ def getByIngredient(ingredient):
     data = requests.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + ingredient)
     if data.status_code == 200 and len(data.text) > 0:
         jsonData = json.loads(data.text)
-        return jsonData['drinks']
+        return jsonData['drinks'] #Returnerer en liste af drinks der indeholder ingrediensen
     else:
         return []
 
@@ -28,14 +28,53 @@ def findDuplicates(listOfLists):
 
 def search(ingredients):
     results = []
+    possibleDrinks = []
     for ingredient in ingredients:
-        results.append(getByIngredient(ingredient))
-    findDuplicates(results)
+        results.append(getByIngredient(ingredient)) #Kalder getByIngredient() for hver ingrediens, og indsætter hver liste i listen results
+    pprint.pprint(results)
+    for result in results:
+        print(len(result))
+    countDict = findDuplicates(results)
+    pprint.pprint(countDict)
+    topscore = max(countDict.values())
+    print(topscore)
+    for id in countDict:
+        if countDict[id] == topscore:
+            print(getById(id))
 
-search(['Gin', 'Vodka'])
+
+
+    """
+    for list in results:
+        for cocktail in list:
+            x = 1
+            possibleDrink = True
+            while x < 15:
+                aCocktail = getById(cocktail['idDrink'])
+                if aCocktail[0]['strIngredient' + str(x)] == None:
+                    if possibleDrink == True:
+                        possibleDrinks.append(cocktail['idDrink'])
+                        x = 20
+                    else:
+                        x = 20
+                elif aCocktail[0]['strIngredient' + str(x)] not in ingredients:
+                    possibleDrink = False
+                    x += 1
+                else:
+                    x += 1
+    noResults = "No cocktails using these ingredients could be found."
+    if len(possibleDrinks) > 0:
+        return possibleDrinks
+    else:
+        return noResults
+    """
+
+print(search(['Vodka', 'Gin']))
 #print(getById('11007'))
 #print(getByIngredient('Vermouth'))
 #print(getByIngredient('Gin'))
 #print('Status Code', data.status_code)
 #print(jsonData)
 #{'strDrink': 'Addison', 'strDrinkThumb': 'https://www.thecocktaildb.com/images/media/drink/yzva7x1504820300.jpg', 'idDrink': '17228'}
+
+#Målet er at brugeren skal kunne skrive alle de ingredienser de har ind, og så få alle de cocktails de kan lave med disse ingredienser. 
---
+++ test.py
@@ -0,0 +1,15 @@
+import requests, json
+
+def getByIngredient(ingredient):
+    data = requests.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + ingredient)
+    if data.status_code == 200 and len(data.text) > 0:
+        jsonData = json.loads(data.text)
+        return jsonData['drinks'] #Returnerer en liste af drinks der indeholder ingrediensen
+    else:
+        return []
+
+print(getByIngredient('Vermouth'))
+"""[{'strDrink': 'Addison', 'strDrinkThumb': 'https://www.thecocktaildb.com/images/media/drink/yzva7x1504820300.jpg', 'idDrink': '17228'}, {'strDrink': "Hunter's Moon", 'strDrinkThumb': 'https://www.thecocktaildb.com/images/media/drink/t0iugg1509556712.jpg', 'idDrink': '17239'}, {'strDrink': 'Ziemes Martini Apfelsaft', 'strDrinkThumb': 'https://www.thecocktaildb.com/images/media/drink/xnzr2p1485619687.jpg', 'idDrink': '14157'}]"""
+
+#print(getById('11007'))
+"""[{'idDrink': '11007', 'strDrink': 'Margarita', 'strDrinkAlternate': None, 'strDrinkES': None, 'strDrinkDE': None, 'strDrinkFR': None, 'strDrinkZH-HANS': None, 'strDrinkZH-HANT': None, 'strTags': 'IBA,ContemporaryClassic', 'strVideo': None, 'strCategory': 'Ordinary Drink', 'strIBA': 'Contemporary Classics', 'strAlcoholic': 'Alcoholic', 'strGlass': 'Cocktail glass', 'strInstructions': 'Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.', 'strInstructionsES': None, 'strInstructionsDE': 'Reiben Sie den Rand des Glases mit der Limettenscheibe, damit das Salz daran haftet. Achten Sie darauf, dass nur der äußere Rand angefeuchtet wird und streuen Sie das Salz darauf. Das Salz sollte sich auf den Lippen des Genießers befinden und niemals in den Cocktail einmischen. Die anderen Zutaten mit Eis schütteln und vorsichtig in das Glas geben.', 'strInstructionsFR': None, 'strInstructionsZH-HANS': None, 'strInstructionsZH-HANT': None, 'strDrinkThumb': 'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg', 'strIngredient1': 'Tequila', 'strIngredient2': 'Triple sec', 'strIngredient3': 'Lime juice', 'strIngredient4': 'Salt', 'strIngredient5': None, 'strIngredient6': None, 'strIngredient7': None, 'strIngredient8': None, 'strIngredient9': None, 'strIngredient10': None, 'strIngredient11': None, 'strIngredient12': None, 'strIngredient13': None, 'strIngredient14': None, 'strIngredient15': None, 'strMeasure1': '1 1/2 oz ', 'strMeasure2': '1/2 oz ', 'strMeasure3': '1 oz ', 'strMeasure4': None, 'strMeasure5': None, 'strMeasure6': None, 'strMeasure7': None, 'strMeasure8': None, 'strMeasure9': None, 'strMeasure10': None, 'strMeasure11': None, 'strMeasure12': None, 'strMeasure13': None, 'strMeasure14': None, 'strMeasure15': None, 'strImageSource': 'https://commons.wikimedia.org/wiki/File:Klassiche_Margarita.jpg', 'strImageAttribution': 'Cocktailmarler', 'strCreativeCommonsConfirmed': 'Yes', 'dateModified': '2015-08-18 14:42:59'}]"""
